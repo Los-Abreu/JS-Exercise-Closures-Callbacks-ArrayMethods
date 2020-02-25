4b0e629330c1b637ea955c38fb1bc494f2ec0798
Carlos Abreu <c.abreu00@gmail.com>
2020-02-25 17:16:17 -0500

    High Order Array Methods Done

--- index.js
+++ index.js
@@ -182,8 +182,12 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let flip= [];
+  runners.forEach(function (runner){
+    return flip.push(`${runner.last_name}, ${runner.first_name}`);
+  });
+  return flip
 }
 
 /**
@@ -198,8 +202,9 @@ function getFullNames(/* CODE HERE */) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  return runners.map(item =>
+     item.first_name.toUpperCase());
 }
 
 /**
@@ -215,8 +220,8 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  return runners.filter(x => x.shirt_size == tShirtSize ? x:null);
 }
 
 /**
@@ -229,8 +234,8 @@ function getRunnersByTShirtSize(/* CODE HERE */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  return runners.reduce((sum, person)=> person.donation + sum, 0);
 }
 
 /////////////// CLOSURES ///////////////
